9051023d32adf7397e1248e4bfd9dec450315c83
Anastasia Ogorodnikova <nasti28121992@icloud.com>
2022-03-14 20:34:35 +0300

    lesson2

--- lesson2/hash1.rb
+++ lesson2/hash1.rb
@@ -1,11 +1,12 @@
 hash = {
   january: 31,
   february: 28,
-   march: 31,
-    april: 30,
-     may: 31
+  march: 31,
+  april: 30,
+  may: 31
 }
 hash.each do |month,days|
   if days == 30
     puts "#{month}"
+end
 end
