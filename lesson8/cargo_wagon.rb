12ac166074348cb242ee47c780d8af6778f3c6dc
nastnine <100627813+nastnine@users.noreply.github.com>
2022-04-24 16:32:54 +0300

    Update cargo_wagon.rb

--- lesson8/cargo_wagon.rb
+++ lesson8/cargo_wagon.rb
@@ -1,17 +1,8 @@
 class CargoWagon < Wagon
-  attr_reader :volume, :occupied_volume
-
-  def initialize(volume)
-    @volume = volume
+  
+  def initialize(space)
+    super
     @type = :cargo
-    @occupied_volume = 0
   end
 
-  def take_volume(volume)
-    @occupied_volume += volume unless free_volume < volume
-  end
-
-  def free_volume
-    @volume - @occupied_volume
-  end
 end
