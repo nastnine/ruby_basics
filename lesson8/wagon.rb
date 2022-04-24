1d5397ea344e0fad7164758adc2b0627fcba66ef
nastnine <100627813+nastnine@users.noreply.github.com>
2022-04-24 16:31:29 +0300

    Update wagon.rb

--- lesson8/wagon.rb
+++ lesson8/wagon.rb
@@ -1,4 +1,17 @@
 class Wagon
   include Manufacturer
-  attr_reader :type
+  attr_reader :type, :space, :taken_space
+
+  def initialize(space)
+    @space = space 
+    @taken_space = 0
+  end 
+
+  def take_space(volume) 
+    @taken_space += volume unless free_space < volume  
+  end 
+
+  def free_space
+    @space - @taken_space
+  end       
 end
